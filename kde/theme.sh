915c156df7b1e396297c1b9fcb6db17bbc3f365b
numanslm <numanslm99@gmail.com>
2023-02-18 21:49:10 +0530

    kde theme

---
+++ kde/theme.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+git clone https://github.com/numanSlm/dotfiles.git
+cd kde
+unzip kde-setup-02.zip
+cd ..
+
+git clone  https://github.com/vinceliuice/Orchis-kde.git
+cd Orchis-kde
+./install.sh
+cd ..
+
+git clone  https://github.com/vinceliuice/Orchis-theme.git
+apt install gtk2-engines-murrine -y
+apt install sassc -y
+cd Orchis-theme
+./install.sh
+cd ..
+
+git clone  https://github.com/vinceliuice/Tela-circle-icon-theme.git
+cd Tela-circle-icon-theme
+./install.sh -a 
+cd ..
+
+git clone  https://github.com/vinceliuice/Vimix-cursors.git
+cd Vimix-cursors
+./install.sh
+cd ..
+
+
+git clone  https://github.com/wsdfhjxc/virtual-desktop-bar.git
+cd virtual-desktop-bar
+./install.sh
