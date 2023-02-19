febe3aa2f2de40017ef299c1cd74a2d904760431
kal <numanslm99@gmail.com>
2023-02-19 12:57:17 +0530

    automated

--- env.sh
+++ env.sh
@@ -1,3 +1,13 @@
+#install all kde/env fonts manually first 
+
+cp ~/Apps/dotfiles/.zshrc ~/.zshrc
+
+cp ~/Apps/dotfiles/.zsh_alias  ~/.zsh_alias
+
+cp ~/Apps/dotfiles/.p10k.zsh  ~/.p10k.zsh
+
+cp ~/Apps/dotfiles/.vimrc  ~/.vimrc
+
 #Git
 sudo apt install git
 
@@ -8,25 +18,24 @@ sudo apt install zsh
 #OhMyZsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/.zsh/powerlevel10k
+echo 'source ~/.zsh/powerlevel10k/powerlevel10k.zsh-theme' >>~/.zshrc
+
+
+# ...or wherever you keep your Git repos/Zsh plugins
+git clone --depth 1 -- https://github.com/marlonrichert/zsh-autocomplete.git ~/.zsh/zsh-autocomplete
+echo 'source ~/.zsh/zsh-autocomplete/zsh-autocomplete.plugin.zsh' >>~/.zshrc
+
+
+git clone https://github.com/zsh-users/zsh-autosuggestions ~/.zsh/zsh-autosuggestions
+echo 'source ~/.zsh/zsh-autosuggestions/zsh-autosuggestions.zsh' >>~/.zshrc
+
 cd ~
 cd Apps
 cd dotfiles
 
 
-sudo cp -R ~/Apps/dotfiles/powerlevel10k/ ~/Apps/ 
 
-sudo cp -R ~/Apps/dotfiles/zsh-autocomplete/ ~/Apps/ 
-
-sudo cp -R ~/Apps/dotfiles/zsh-autosuggestions/ ~/Apps/
-
-#rc files
-cp ~/Apps/dotfiles/.zshrc ~/.zshrc
-
-cp ~/Apps/dotfiles/.zsh_alias  ~/.zsh_alias
-
-cp ~/Apps/dotfiles/.p10k.zsh  ~/.p10k.zsh
-
-cp ~/Apps/dotfiles/.vimrc  ~/.vimrc
 
 #disc
 #chmod +x discord_appimage.sh 
